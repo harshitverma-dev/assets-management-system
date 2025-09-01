320817d0cf8089063eb43f3186ede91ce4e62a98
harshitverma-dev <theharshitverma53@gmail.com>
2025-09-02 00:14:20 +0530

    deployed on vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://68b5a555e5dc090291afc730.mockapi.io/api/v1/', // Example mockapi.io base URL
+  baseURL: 'https://68b5a555e5dc090291afc730.mockapi.io/api/v1/', 
   headers: {
     'Content-Type': 'application/json',
   },
